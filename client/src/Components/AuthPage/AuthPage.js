e5e26d3d3c625b4705c9b777b91e0e37f7204938
Konstantin Postoian <hamelltonn@gmail.com>
2020-12-07 16:16:23 +0200

    create form, edit style form, edit navbar style, create module system css for component, rename titile

--- client/src/Components/AuthPage/AuthPage.js
+++ client/src/Components/AuthPage/AuthPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar } from '../Navbar/Navbar'
+import classes from './AuthPage.module.css'
 
 export let AuthPage = () => {
     return (
@@ -10,13 +11,34 @@ export let AuthPage = () => {
                     <h1>Certiport</h1>
                     <div className="card blue-grey darken-1">
                         <div className="card-content white-text">
-                            <span className="card-title">Card Title</span>
-                            <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+                            <span className="card-title">Авторизация</span>
+                            <div>
+                                <div className="input-field">
+                                    <input 
+                                    placeholder="email" 
+                                    id="email" 
+                                    type="text" 
+                                    className="validate" 
+                                    name="email"
+                                    className="yellow-input"
+                                    />
+                                    <label htmlFor="first_name">Email</label>
+                                </div>
+                                <div className="input-field">
+                                    <input 
+                                    placeholder="password" 
+                                    id="password" 
+                                    type="password" 
+                                    name="password"
+                                    className="yellow-input"
+                                    />
+                                    <label htmlFor="password">Пароль</label>
+                                </div>
+                            </div>
                         </div>
                         <div className="card-action">
-                            <a href="#">This is a link</a>
-                            <a href="#">This is a link</a>
+                            <button className={`${classes.margin} btn yellow darken-4`}>Войти</button>
+                            <button className="btn grey lighten-1 black-text">Войти</button>
                         </div>
                     </div>
                 </div>
--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import classes from './Navbar.module.css'
 
 export let Navbar = () => {
     return (
         <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo right">Logo</a>
-                <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <div className="nav-wrapper  light-blue darken-3">
+                <a href="#" className="brand-logo right"><h6 className={classes.margin_top}>Тел: +38 (050) 435 32 91. E-mail: certiport@dinternal.com.ua </h6></a>
+                <ul id="nav-mobile" className="left">
                     <li><a href="/">Реєстрація</a></li>
                     <li><a href="/">Ціни</a></li>
                     <li><a href="/">Контакти</a></li>
